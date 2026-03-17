f810b849d5a1bdfe7f8d20dd612594bb80dded5a
MohanaKumar1309 <MohanaKumar.K@cognizant.com>
2026-03-17 16:02:39 +0530

    Added enity, repository and enums in the campaign modues

--- src/main/java/com/cts/repository/CampaignRepository.java
+++ src/main/java/com/cts/repository/CampaignRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cts.entity.Campaign;
+import com.cts.enums.ApprovalStatus;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
     List<Campaign> findByStatusAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
