c299001843bd0de534d8ac240e3e03eff337419a
G Padmini <pdmgk05@gmail.com>
2026-03-17 16:09:25 +0530

    Product entity added....

---
+++ src/main/java/com/cts/entity/Product.java
@@ -0,0 +1,31 @@
+package com.cts.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private String name;
+    private String sku;
+    private BigDecimal price;
+    private String imageUrl;
+
+}
