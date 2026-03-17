267055e21e141e5885121c685a6834a2dffb889a
Shaarmi <shaarmi947@gmail.com>
2026-03-17 15:37:24 +0530

    entites added

--- src/main/java/com/cts/entity/Redemption.java
+++ src/main/java/com/cts/entity/Redemption.java
@@ -1,4 +1,36 @@
 package com.cts.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Redemption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long redemptionId;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Order order;
+
+    @Lob
+    private String promotionIds;
+
+    @Lob
+    private String campaignIds;
+
+    private String couponCode;
+
+    private BigDecimal discountAmount;
+    private BigDecimal finalAmount;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
