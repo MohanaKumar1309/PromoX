e60eeb12935f7a951bbb33d0c7b4ed8609265715
G Padmini <pdmgk05@gmail.com>
2026-03-18 16:46:52 +0530

    product repository

---
+++ src/main/java/com/cts/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.cts.repository;
+
+import com.cts.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByNameContainingIgnoreCase(String keyword);
+    List<Product> findByCategory_CategoryId(Long categoryId);
+}
