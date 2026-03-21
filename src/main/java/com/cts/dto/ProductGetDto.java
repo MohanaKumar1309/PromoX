ddef0c63a51df5a708cdcfd04f56db0d4cb2d45c
G Padmini <pdmgk05@gmail.com>
2026-03-21 20:19:04 +0530

    added productgetdto

---
+++ src/main/java/com/cts/dto/ProductGetDto.java
@@ -0,0 +1,18 @@
+package com.cts.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class ProductGetDto {
+    private Long productId;
+    private Long categoryId;
+    private String categoryName;
+    private String name;
+    private String sku;
+    private BigDecimal price;
+    private String imageUrl;
+}
