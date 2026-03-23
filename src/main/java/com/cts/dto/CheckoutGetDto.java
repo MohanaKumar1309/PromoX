b5257fb24f2d7f1d25615afd77bdc5a1c6a8b013
Shaarmi <shaarmi947@gmail.com>
2026-03-23 11:42:52 +0530

    changes made

--- src/main/java/com/cts/dto/CheckoutGetDto.java
+++ src/main/java/com/cts/dto/CheckoutGetDto.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 @Builder
-public class CheckoutResponse {
+public class CheckoutGetDto {
     private Long orderId;
     private BigDecimal totalAmount;
     private BigDecimal discountAmount;
--- src/main/java/com/cts/entity/Redemption.java
+++ src/main/java/com/cts/entity/Redemption.java
@@ -1,10 +1,11 @@
 package com.cts.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-
+@Data
 public class Redemption {
 
     @Id
